ece3b9fcb0653a3595a2061f39740d49337d6cb4
James Fulford <james.fulford@outlook.com>
2019-03-13 01:01:27 +0000

    WIP(train): Improved model architecture

--- clone.py
+++ clone.py
@@ -1,31 +1,68 @@
 import csv
 import os
 import numpy as np
+import cv2
 from scipy import ndimage
 
+#
+# Reading data
+#
 lines = []
 data_dir = "/opt/carnd_p3/data"
 with open(os.path.join(data_dir, "driving_log.csv")) as phile:
     lines = list(csv.reader(phile))[1:]
 
 center_image_paths = list(map(lambda l: os.path.join(data_dir, "IMG", os.path.basename(l[0])), lines))
-X_train = np.array(list(map(
-    lambda i: ndimage.imread(i),
-    center_image_paths
-)))
-y_train = np.array(list(map(
+center_images = list(map(ndimage.imread, center_image_paths))
+
+output_values = list(map(
     lambda l: float(l[3]),
     lines
-)))
+))
+
+#
+# Building training sets
+#
+X_train = np.array(
+    center_images  # original data
+    + list(map(np.fliplr, center_images))  # horizontal flip
+)
+
+y_train = np.array(
+    output_values  # original data
+    + list(map(lambda y: -y, output_values))  # horizontal flip
+)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Activation, Flatten, Dense, Lambda, Convolution2D, pooling
 
 input_shape = (160, 320, 3)
 
 model = Sequential()
 model.add(Lambda(lambda i: (i - 128) / 256., input_shape=input_shape))
+
+model.add(Convolution2D(20, 5, 5))
+model.add(Activation("relu"))
+model.add(pooling.MaxPooling2D())
+
+model.add(Convolution2D(15, 5, 5))
+model.add(Activation("relu"))
+model.add(pooling.MaxPooling2D())
+                     
 model.add(Flatten())
+
+model.add(Dense(500))
+model.add(Activation("relu"))
+
+model.add(Dense(100))
+model.add(Activation("relu"))
+
+model.add(Dense(20))
+model.add(Activation("relu"))
+
+model.add(Dense(5))
+model.add(Activation("relu"))
+
 model.add(Dense(1))
 
 model.compile(loss="mse", optimizer="adam")
@@ -34,6 +71,6 @@ model.fit(
     y_train,
     validation_split=0.2,
     shuffle=True,
-    nb_epoch=3,
+    epochs=3,
 )
 model.save("model.h5")
