e8e469c0b90416cc6501b4eb27f4ab1e235da721
James Fulford <james.fulford@outlook.com>
2019-03-13 02:38:14 +0000

    WIP(train): Using side images and smaller model

--- clone.py
+++ clone.py
@@ -12,57 +12,49 @@ data_dir = "/opt/carnd_p3/data"
 with open(os.path.join(data_dir, "driving_log.csv")) as phile:
     lines = list(csv.reader(phile))[1:]
 
-center_image_paths = list(map(lambda l: os.path.join(data_dir, "IMG", os.path.basename(l[0])), lines))
-center_images = list(map(ndimage.imread, center_image_paths))
-
-output_values = list(map(
-    lambda l: float(l[3]),
-    lines
-))
-
-#
-# Building training sets
-#
-X_train = np.array(
-    center_images  # original data
-    + list(map(np.fliplr, center_images))  # horizontal flip
+image_paths = (
+    list(map(lambda l: os.path.join(data_dir, "IMG", os.path.basename(l[0])), lines))  # center
+    + list(map(lambda l: os.path.join(data_dir, "IMG", os.path.basename(l[1])), lines))  # left
+    + list(map(lambda l: os.path.join(data_dir, "IMG", os.path.basename(l[2])), lines))  # right
 )
-
+all_images = list(map(ndimage.imread, image_paths))
+center_values = list(map(lambda l: float(l[3]), lines))
+correction = .2
+all_values = (
+    center_values  # center
+    + list(map(lambda y: y + correction, center_values))  # left
+    + list(map(lambda y: y - correction, center_values))  # right
+)
+                  
+X_train = np.array(
+    all_images
+    + list(map(np.fliplr, all_images))
+)
 y_train = np.array(
-    output_values  # original data
-    + list(map(lambda y: -y, output_values))  # horizontal flip
+    all_values
+    + list(map(lambda y: -y, all_values))
 )
 
 from keras.models import Sequential
-from keras.layers import Activation, Flatten, Dense, Lambda, Convolution2D, pooling
+from keras.layers import Cropping2D, Flatten, Dense, Lambda, Convolution2D, pooling
 
 input_shape = (160, 320, 3)
 
 model = Sequential()
-model.add(Lambda(lambda i: (i - 128) / 256., input_shape=input_shape))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=input_shape))
+model.add(Lambda(lambda i: (i - 128) / 256.))
 
-model.add(Convolution2D(20, 5, 5))
-model.add(Activation("relu"))
+model.add(Convolution2D(10, 5, 5, activation="relu"))
 model.add(pooling.MaxPooling2D())
 
-model.add(Convolution2D(15, 5, 5))
-model.add(Activation("relu"))
+model.add(Convolution2D(6, 5, 5, activation="relu"))
 model.add(pooling.MaxPooling2D())
-                     
+
 model.add(Flatten())
 
-model.add(Dense(500))
-model.add(Activation("relu"))
-
+model.add(Dense(400))
 model.add(Dense(100))
-model.add(Activation("relu"))
-
-model.add(Dense(20))
-model.add(Activation("relu"))
-
-model.add(Dense(5))
-model.add(Activation("relu"))
-
+model.add(Dense(25))
 model.add(Dense(1))
 
 model.compile(loss="mse", optimizer="adam")
